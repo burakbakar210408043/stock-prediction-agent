c8dfa2874bb08b32f4091047aa30ddf80e144904
burakbakar210408043 <210408043@st.biruni.edu.tr>
2026-01-20 20:04:09 +0300

    Baslatici (bat) eklendi ve README guncellendi

---
+++ .bat
@@ -0,0 +1,14 @@
+@echo off
+echo ==========================================
+echo   BORSA AJANI BASLATILIYOR...
+echo   Lutfen tarayicinin acilmasini bekle.
+echo ==========================================
+
+:: Kodun oldugu klasore git (Hata almamak icin garanti yontem)
+cd /d "%~dp0"
+
+:: Uygulamayi calistir
+py -m streamlit run main.py
+
+:: Eger hata verirse pencere hemen kapanmasin diye bekle
+pause
